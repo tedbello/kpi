bb1f3e05f130106452f3ac845fc6397fe2159827
tedbello <ted.bello@gmail.com>
2024-08-27 01:45:03 -0400

    Start Writing the XSL export 1st scenario

--- ui_auto_tests/cypress/e2e/00-New-tests/exports-tests/export-xls-with-1-question.spec.js
+++ ui_auto_tests/cypress/e2e/00-New-tests/exports-tests/export-xls-with-1-question.spec.js
@@ -1,5 +1,9 @@
  /// <reference types="cypress" /> 
 
+import {ListOfSurveyPage} from "../../pom/projectListPage";
+import {SurveyDetailPage} from "../../pom/surveyPage";
+
+
 describe('Can export project tp XLS.', () => {
     before(() => {
         cy.fixture('accounts')
@@ -7,11 +11,45 @@ describe('Can export project tp XLS.', () => {
         .then(($acct) => {
             cy.login($acct, 'super_admin')
         })
+
+        //Assume survey exists and is accessible to the user: Need to import a survey with 1 question
+        // OR create Before to load it into the database
     })
 
-    it('In order to export a form in XLS, user should exist', () => {
-        cy.log("In the Export first test")
+    it.only('User can export a form in XLS, ', () => {
+        cy.log(`Scenarios:
+                Given the survery/form Survey_1 exists
+                When the user export the servey in 'XLS' format
+                Then the survey should have been downloaded`);
+
+        const surveyListPage = new ListOfSurveyPage();
+        const surveyDetailPage = new SurveyDetailPage();
+
+        //Given the survery/form "Survey 1" exists
+        surveyListPage.OpenSurveyByName("survey 1");
+        surveyDetailPage.openSurveySubPageByName("data");
+
+        //When the user export the servey in 'XLS' format
+        surveyDetailPage.exportSurveyinFormat('XSL');
+
+        //Then the form should have been downloaded 
     })
+
+
+    it('User can validate questions and responses within downloaded form, ', () => {
+        cy.log(`Steps: \n
+                Given the survery/form Survey_2 exists \n
+                And has been download \n
+                When the user opens the download survey \n
+                Then he should see 2 column in the file with titles 'Questions' and '_uuid' \n
+                And he should see the response '???' under the 'question' column \n
+                And He should see the response '/blank/ ' under the '_uuid' columne`)
+    })
+
+    after(() => {
+        cy.log("Need to remove the surveys/forms and users if they will be required for another tests!")
+        }
+    )
 
 
 });
